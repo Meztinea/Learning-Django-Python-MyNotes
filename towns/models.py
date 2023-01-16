a1840322fd92980d50dd1f3cf37067ee94d73666
Raquel <meztinea.dev@gmail.com>
2023-01-16 09:33:57 -0600

    Agrega el primer modelo
    Se crea el primer modelo de la app, se registra en el archivo
    admin de la app, se agrega la app towns al proyecto en el archivo
    settings. Se crean y aplican las migraciones. Se agregan más notas
    personales.

--- towns/models.py
+++ towns/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Town(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='towns/photos')
+    population = models.IntegerField()
+    country = models.CharField(max_length=100)
+    
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
